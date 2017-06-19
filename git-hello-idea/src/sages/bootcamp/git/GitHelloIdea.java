de190555228a73924f0f16efd4869af8ab8c5e6c
Arkadiusz Jagura <arkadiusz.jagura@e-post.pl>
2017-06-19 14:57:55 +0200

    Kolejna zmiana

---
+++ git-hello-idea/src/sages/bootcamp/git/GitHelloIdea.java
@@ -0,0 +1,10 @@
+package sages.bootcamp.git;
+
+/**
+ * Created by Jag on 2017-06-19.
+ */
+public class GitHelloIdea {
+    public static void main(String[] args) {
+        System.out.println("Hello hello");
+    }
+}
